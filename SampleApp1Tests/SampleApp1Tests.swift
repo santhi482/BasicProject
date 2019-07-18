e4e9fa79670972804112f0a7a26c3adafc00fc56
Santhi482 <santhipriya.a@abhibus.com>
2019-07-18 16:05:07 +0530

    Initial Commit

---
+++ SampleApp1Tests/SampleApp1Tests.swift
@@ -0,0 +1,34 @@
+//
+//  SampleApp1Tests.swift
+//  SampleApp1Tests
+//
+//  Created by Santhi Priya  on 18/07/19.
+//  Copyright © 2019 Santhi Priya . All rights reserved.
+//
+
+import XCTest
+@testable import SampleApp1
+
+class SampleApp1Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
